750268ad7bd563267f64c5a84e20a0d5f2f56019
p2lifau <p2lifau@gmail.com>
2022-03-30 10:35:18 -0700

    edited models

--- models/item.js
+++ models/item.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // this our schema which wel use to access data, properties when we incorporate the api
 const itemSchema = new Schema({
-    productName: {type: String, unique: true, required: true},
-    quantity: {type: Number, default: 0, required: true}
+    shoeName: {type: String, unique: true, required: true},
+    brand: {type: String, required: true},
+    img: {type: String},
+    description: {type: String}
 }, {timeStamps: true})
 
 const Item = mongoose.model('Item', itemSchema);
